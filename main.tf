d569e282c7a9b3e86e073e8d54365084217f3106
Your Name <you@example.com>
2025-11-07 18:52:43 +0000

    Jay Project

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_security_group" "zander_security_group" {
+resource "aws_security_group" "jay_security_group" {
   ingress {
     from_port   = 80
     to_port     = 80
@@ -42,7 +42,7 @@ resource "aws_key_pair" "zander_key" {
 resource "aws_instance" "jay_server" {
   ami             = "ami-0c5204531f799e0c6"
   instance_type   = "t3.micro"
-  security_groups = [aws_security_group.zander_security_group.name]
+  security_groups = [aws_security_group.jay_security_group.name]
   key_name        = aws_key_pair.zander_key.key_name
 
   tags = {
@@ -50,7 +50,7 @@ resource "aws_instance" "jay_server" {
   }
 
   provisioner "local-exec" {
-    command = "sleep 30 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${self.public_ip},' -u ec2-user --private-key ~/.ssh/jaykey deploy.yml"
+    command = "sleep 30 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${self.public_ip},' -u ec2-user --private-key jaykey deploy.yml"
   }
 }
 
